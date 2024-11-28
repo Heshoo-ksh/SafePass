9260fd24f3d6125729d6889945f1e1d94cc283c2
Heshoo-ksh <hishamodeh2001@gmail.com>
2024-11-28 16:59:45 -0500

    Add login page

--- SafePass/Data/Login.cs
+++ SafePass/Data/Login.cs
@@ -8,7 +8,7 @@ namespace SafePass.Data
           public Guid Id { get; set; }
 
           [Required(ErrorMessage = "Username is required and cannot be empty.")]
-          public string? UserName { get; set; }
+          public string? Username { get; set; }
 
           [Required(ErrorMessage = "Password is required and cannot be empty.")]
           public string? Password { get; set; }
