570afe1aa7c2c0a0c9bc34e6a0f2c206d6eca60d
moe <msalma@umich.edu>
2024-11-29 21:37:40 -0500

    fixed login page

--- SafePass/Program.cs
+++ SafePass/Program.cs
@@ -2,26 +2,28 @@ using SafePass.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.EntityFrameworkCore;
+using MudBlazor.Services; // MudBlazor services
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddMudServices();
 builder.Services.AddSingleton<WeatherForecastService>();
-
-// Add this line to register controllers
 builder.Services.AddControllers();
 
 // Configure SQLite database context.
 builder.Services.AddDbContext<UserContext>(options =>
-    options.UseSqlite("Data Source=users.db")); // Specify your SQLite database file name
+    options.UseSqlite("Data Source=users.db"));
 
 // Register the UserService.
 builder.Services.AddScoped<UserService>();
-
-// Add HttpClient service (we'll need this for API calls)
 builder.Services.AddHttpClient();
+
+// Register MudBlazor services
+
 
 var app = builder.Build();
 
@@ -29,18 +31,14 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
 
-// Add this line to map controllers
 app.MapControllers();
-
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
