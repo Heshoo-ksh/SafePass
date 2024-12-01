1b54e2319b5e20bead4bcc9c1f0a45f62dbfe609
moe <msalma@umich.edu>
2024-12-01 17:00:58 -0500

    added .ignore files back

---
+++ SafePass/Services/AuthState.cs
@@ -0,0 +1,28 @@
+﻿
+namespace SafePass.Services
+{
+    public class AuthState
+    {
+        public bool IsAuthenticated { get; private set; }
+        public string? UserName { get; private set; }
+
+        public event Action? OnAuthStateChanged;
+
+        public void LogIn(string userName)
+        {
+            IsAuthenticated = true;
+            UserName = userName;
+            NotifyAuthStateChanged();
+        }
+
+        public void LogOut()
+        {
+            IsAuthenticated = false;
+            UserName = null;
+            NotifyAuthStateChanged();
+        }
+
+        private void NotifyAuthStateChanged() => OnAuthStateChanged?.Invoke();
+    }
+}
+
