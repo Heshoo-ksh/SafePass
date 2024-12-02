cba4138f9e575417f06d3f07600c65fc69e35646
Heshoo-ksh <hishamodeh2001@gmail.com>
2024-12-01 20:54:52 -0500

    Add credir card classes

---
+++ SafePass/Pages/CreditCardVault.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SafePass.Pages
+{
+    public class CreditCardVaultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
