63c137e13daac7238acc471dd4ad2fb2b6d67799
Heshoo-ksh <hishamodeh2001@gmail.com>
2024-12-01 21:06:34 -0500

    Add ignored classes

---
+++ SafePass/Proxies/CreditCardProxy.cs
@@ -0,0 +1,42 @@
+﻿using SafePass.Data;
+
+namespace SafePass.Proxies
+{
+     public class CreditCardProxy
+     {
+          private CreditCard _creditCard;
+          public bool IsUnmasked { get; set; } = false;
+
+          public CreditCardProxy(CreditCard creditCard)
+          {
+               _creditCard = creditCard;
+          }
+
+          public Guid Id => _creditCard.Id;
+
+          public string? Name => _creditCard.Name;
+
+          public string? CardHolderName => IsUnmasked ? _creditCard.CardHolderName : new string('*', _creditCard.CardHolderName.Length);
+
+          public string? Number => IsUnmasked ? _creditCard.Number : MaskCardNumber(_creditCard.Number);
+
+          public string? CVV => IsUnmasked ? _creditCard.CVV : new string('*', _creditCard.CVV.Length);
+
+          public string? ZipCode => IsUnmasked ? _creditCard.ZipCode : new string('*', _creditCard.ZipCode.Length);
+
+          public DateTime? ExpirationDate => _creditCard.ExpirationDate;
+
+          // Helper methods to mask sensitive data
+          private string MaskCardNumber(string? number)
+          {
+               if (string.IsNullOrEmpty(number))
+                    return string.Empty;
+
+               if (number.Length <= 4)
+                    return new string('*', number.Length);
+
+               // Show only last 4 digits
+               return new string('*', number.Length - 4) + number.Substring(number.Length - 4);
+          }
+     }
+}
---
+++ SafePass/Services/CreditCardService.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SafePass.Data;
+
+namespace SafePass.Services
+{
+     public class CreditCardService
+     {
+          private readonly IDbContextFactory<SafePassContext> _dbContextFactory;
+
+          public CreditCardService(IDbContextFactory<SafePassContext> dbContextFactory)
+          {
+               _dbContextFactory = dbContextFactory;
+          }
+
+          public async Task CreateCreditCard(CreditCard creditCard)
+          {
+               using (var context = _dbContextFactory.CreateDbContext())
+               {
+                    context.CreditCards.Add(creditCard);
+                    await context.SaveChangesAsync();
+               }
+          }
+
+          public async Task<CreditCard?> GetCreditCardById(Guid id)
+          {
+               using (var context = _dbContextFactory.CreateDbContext())
+               {
+                    return await context.CreditCards.FindAsync(id);
+               }
+          }
+
+          public async Task<CreditCard[]> GetAllCreditCards()
+          {
+               using (var context = _dbContextFactory.CreateDbContext())
+               {
+                    return await context.CreditCards.ToArrayAsync();
+               }
+          }
+
+          public async Task UpdateCreditCard(CreditCard creditCard)
+          {
+               using (var context = _dbContextFactory.CreateDbContext())
+               {
+                    context.CreditCards.Update(creditCard);
+                    await context.SaveChangesAsync();
+               }
+          }
+
+          public async Task DeleteCreditCard(Guid id)
+          {
+               using (var context = _dbContextFactory.CreateDbContext())
+               {
+                    var creditCard = await context.CreditCards.FindAsync(id);
+                    if (creditCard != null)
+                    {
+                         context.CreditCards.Remove(creditCard);
+                         await context.SaveChangesAsync();
+                    }
+               }
+          }
+     }
+}
