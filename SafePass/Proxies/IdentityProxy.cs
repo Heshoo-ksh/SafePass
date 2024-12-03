5147a280f372c60da08421baa02f181474a55d63
Heshoo-ksh <hishamodeh2001@gmail.com>
2024-12-02 22:08:34 -0500

    Add identity page

---
+++ SafePass/Proxies/IdentityProxy.cs
@@ -0,0 +1,41 @@
+﻿using SafePass.Data;
+
+namespace SafePass.Proxies
+{
+     public class IdentityProxy
+     {
+          private Identity _identity;
+          public bool IsUnmasked { get; set; } = false;
+
+          public IdentityProxy(Identity identity)
+          {
+               _identity = identity;
+          }
+
+          public Guid Id => _identity.Id;
+
+          public string HolderName => _identity.HolderName;
+
+          public string IDNumber => IsUnmasked ? _identity.IDNumber : MaskString(_identity.IDNumber);
+
+          public string IDType => _identity.IDType;
+
+          public DateOnly? ExpirationDate => _identity.ExpirationDate;
+
+          public DateOnly? IssueDate => _identity.IssueDate;
+
+          public DateOnly? DateOfBirth => _identity.DateOfBirth;
+
+          public string Gender => _identity.Gender;
+
+          public string Address => IsUnmasked ? _identity.Address : MaskString(_identity.Address);
+
+          private string MaskString(string value)
+          {
+               if (string.IsNullOrEmpty(value))
+                    return string.Empty;
+
+               return new string('*', value.Length);
+          }
+     }
+}
---
+++ SafePass/Services/IdentityService.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SafePass.Data;
+
+namespace SafePass.Services
+{
+     public class IdentityService
+     {
+          private readonly IDbContextFactory<SafePassContext> _dbContextFactory;
+
+          public IdentityService(IDbContextFactory<SafePassContext> dbContextFactory)
+          {
+               _dbContextFactory = dbContextFactory;
+          }
+
+          public async Task CreateIdentity(Identity identity)
+          {
+               using (var context = _dbContextFactory.CreateDbContext())
+               {
+                    context.Identities.Add(identity);
+                    await context.SaveChangesAsync();
+               }
+          }
+
+          public async Task<Identity?> GetIdentityById(Guid id)
+          {
+               using (var context = _dbContextFactory.CreateDbContext())
+               {
+                    return await context.Identities.FindAsync(id);
+               }
+          }
+
+          public async Task<Identity[]> GetAllIdentities()
+          {
+               using (var context = _dbContextFactory.CreateDbContext())
+               {
+                    return await context.Identities.ToArrayAsync();
+               }
+          }
+
+          public async Task UpdateIdentity(Identity identity)
+          {
+               using (var context = _dbContextFactory.CreateDbContext())
+               {
+                    context.Identities.Update(identity);
+                    await context.SaveChangesAsync();
+               }
+          }
+
+          public async Task DeleteIdentity(Guid id)
+          {
+               using (var context = _dbContextFactory.CreateDbContext())
+               {
+                    var identity = await context.Identities.FindAsync(id);
+                    if (identity != null)
+                    {
+                         context.Identities.Remove(identity);
+                         await context.SaveChangesAsync();
+                    }
+               }
+          }
+     }
+}
