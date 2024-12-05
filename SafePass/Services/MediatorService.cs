b40d37e330713bd31a938d63b8296c06f2063fbf
moe <msalma@umich.edu>
2024-12-04 21:00:51 -0500

    implemented master pass health widget

---
+++ SafePass/Services/MediatorService.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace SafePass.Services
+{
+    public class MediatorService : IMediatorService
+    {
+        private readonly Dictionary<string, List<Action<object>>> _callbacks = new();
+
+        public void Notify(string eventName, object data = null)
+        {
+            if (_callbacks.ContainsKey(eventName))
+            {
+                foreach (var callback in _callbacks[eventName])
+                {
+                    callback.Invoke(data);
+                }
+            }
+        }
+
+        public void Register(string eventName, Action<object> callback)
+        {
+            if (!_callbacks.ContainsKey(eventName))
+            {
+                _callbacks[eventName] = new List<Action<object>>();
+            }
+            _callbacks[eventName].Add(callback);
+        }
+    }
+}
